8db15041d36c8926f377253ca400c63c98daf2c8
Ruben Ernst <4404015+Ruben-E@users.noreply.github.com>
2021-01-16 11:48:05 +0100

    Added schedule for check and prune

--- backup.js
+++ backup.js
@@ -3,6 +3,7 @@ const winston = require('winston');
 const cron = require('node-cron');
 const Transport = require('winston-transport');
 const Discord = require('discord.js');
+var getISOWeek = require('date-fns/getISOWeek')
 
 require('winston-daily-rotate-file');
 
@@ -72,16 +73,43 @@ const args = process.argv.slice(2);
 const discordHook = new Discord.WebhookClient(process.env.DISCORD_WEBHOOK_ID, process.env.DISCORD_WEBHOOK_TOKEN);
 const discordCharacterLimit = 1900;
 
-const cronSchedule = process.env.BACKUP_CRON || '0 */6 * * *'
+const backupCron = process.env.BACKUP_CRON || '0 */6 * * *'
+const checkCron = process.env.CHECK_CRON || '0 7 * * 1'
+const pruneCron = process.env.CHECK_CRON || '0 1 1 * *'
 
 const backupArgs = process.env.RESTIC_JOB_ARGS || '';
 const forgetArgs = process.env.RESTIC_FORGET_ARGS || '';
+const checkArgs = process.env.RESTIC_CHECK_ARGS || '';
+const pruneArgs = process.env.RESTIC_PRUNE_ARGS || '';
 
 const rcloneArgs = `serve restic --stdio --b2-hard-delete --drive-use-trash=false --fast-list --transfers=32`
 
 const backupCommand = `restic backup /data ${backupArgs} -o rclone.args="${rcloneArgs}"`
 const forgetCommand = `restic forget ${forgetArgs} -o rclone.args="${rcloneArgs}"`
-const unlockCommand = `restic unlock -o rclone.args="${rcloneArgs}"`
+const checkWeeklySubsetCommand = `restic check ${checkArgs} --read-data-subset=<week>/52 -o rclone.args="${rcloneArgs}"`
+const pruneCommand = `restic prune ${pruneArgs} -o rclone.args="${rcloneArgs}"`
+
+function notifyDiscord() {
+    try {
+        const logs = collectorTransport.getCollection()
+        collectorTransport.resetCollection()
+
+        let buffer = []
+        for (const log of logs) {
+            const out = `${buffer.join('\n')}\n${log}`
+            if (out.length >= discordCharacterLimit) {
+                discordHook.send(`\`\`\`${buffer.join('\n')}\`\`\``);
+                buffer = [];
+            }
+
+            buffer.push(log)
+        }
+
+        discordHook.send(`\`\`\`${buffer.join('\n')}\`\`\``);
+    } catch (e) {
+        logger.error(`Discord notification failed (${e.code}): ${e.message}`)
+    }
+}
 
 async function runAndLogRestic(cmd) {
     logger.info(`Running '${cmd}'`)
@@ -95,10 +123,6 @@ async function runAndLogRestic(cmd) {
     }
 }
 
-async function unlock() {
-    await runAndLogRestic(unlockCommand);
-}
-
 async function backup() {
     await runAndLogRestic(backupCommand);
 }
@@ -107,25 +131,7 @@ async function forget() {
     await runAndLogRestic(forgetCommand);
 }
 
-function notifyDiscord() {
-    const logs = collectorTransport.getCollection()
-    collectorTransport.resetCollection()
-
-    let buffer = []
-    for (const log of logs) {
-        const out = `${buffer.join('\n')}\n${log}`
-        if (out.length >= discordCharacterLimit) {
-            discordHook.send(`\`\`\`${buffer.join('\n')}\`\`\``);
-            buffer = [];
-        }
-
-        buffer.push(log)
-    }
-
-    discordHook.send(`\`\`\`${buffer.join('\n')}\`\`\``);
-}
-
-async function main() {
+async function backupAndForget() {
     try {
         await backup();
         logger.info(`Backup succesful`);
@@ -133,25 +139,77 @@ async function main() {
         logger.info('Forget succesful')
     } catch (e) {
         logger.warn(`Backup / forget failed (${e.code}): ${e.message}`)
-        try {
-            await unlock()
-        } catch (e) {
-            logger.warn(`Unlock failed (${e.code}): ${e.message}`)
-        }
     }
 
     notifyDiscord();
 }
 
-if (args.length > 0 && args[0] == 'cron') {
-    cron.schedule(cronSchedule, () => {
-        main()
+async function checkWeeklySubset() {
+    try {
+        let week = getISOWeek(new Date(), { weekStartsOn: 1 })
+
+        if (week > 52) {
+            week = 52;
+        }
+
+        const cmd = checkWeeklySubsetCommand.replace('<week>', week)
+        await runAndLogRestic(cmd)
+        logger.info(`Check succesful`);
+    } catch (e) {
+        logger.warn(`Check failed (${e.code}): ${e.message}`)
+    }
+
+    notifyDiscord();
+}
+
+async function prune() {
+    try {
+        await runAndLogRestic(pruneCommand);
+        logger.info(`Prune succesful`);
+    } catch (e) {
+        logger.warn(`Prune failed (${e.code}): ${e.message}`)
+    }
+
+    notifyDiscord();
+}
+
+let job = 'backup';
+if (args.length > 0) {
+    job = args[0]
+}
+
+if (job == 'cron') {
+    cron.schedule(backupCron, () => {
+        backupAndForget()
     });
 
-    logger.info(`Application started with schedule '${cronSchedule}'`)
-} else {
+    cron.schedule(checkCron, () => {
+        checkWeeklySubset()
+    });
+
+    cron.schedule(pruneCron, () => {
+        prune()
+    });
+
+    logger.info(`Application started`)
+    logger.info(`Backup cron '${backupCron}'`)
+    logger.info(`Check cron '${checkCron}'`)
+    logger.info(`Prune cron '${pruneCron}'`)
+} else if (job == 'backup') {
     (async () => {
-        await main();
+        await backupAndForget();
         discordHook.destroy();
     })();
+} else if (job == 'check') {
+    (async () => {
+        await checkWeeklySubset();
+        discordHook.destroy();
+    })();
+} else if (job == 'prune') {
+    (async () => {
+        await prune();
+        discordHook.destroy();
+    })();
+} else {
+    logger.error(`Unknown command '${job}'`)
 }
